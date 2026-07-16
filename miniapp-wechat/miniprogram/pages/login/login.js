f9264529be4eafcd557f9eff1f91f6bc30e70721
xxy-lh <1404900576@qq.com>
2026-07-16 18:36:53 +0800

    完成微信小程序初版

--- miniapp-wechat/miniprogram/pages/login/login.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const auth_1 = require("../../services/auth");
-Page({ login() { wx.showLoading({ title: '登录中' }); (0, auth_1.ensureLogin)().catch(error => wx.showToast({ title: error.message, icon: 'none' })).finally(() => wx.hideLoading()); } });
